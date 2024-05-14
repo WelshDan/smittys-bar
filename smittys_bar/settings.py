5899e94c140b3ad0739b489c70f87ff33283a86f
WelshDan <dan.roberts78@gmail.com>
2024-05-14 21:50:05 +0000

    Edit file name from images to media to help cloudinary connection

--- smittys_bar/settings.py
+++ smittys_bar/settings.py
@@ -159,14 +159,15 @@ TIME_FORMAT: '%H:%M'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
