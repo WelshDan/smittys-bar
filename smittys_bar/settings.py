9633784ef75be72aa6e4fe0bc94d4781f0923fc3
WelshDan <dan.roberts78@gmail.com>
2024-09-11 11:06:41 +0000

    Correction: Undo previous remove old bookings function

--- smittys_bar/settings.py
+++ smittys_bar/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = 'True'
 
 ALLOWED_HOSTS = [
                 '8000-welshdan-smittysbar-8dbkmoy0e5m.ws-eu116.gitpod.io',
